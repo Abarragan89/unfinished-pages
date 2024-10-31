00634186ee0ea6379d7e55835acc39773b615cca
Anthony Barragan <anthony.bar.89@gmail.com>
2024-10-31 08:09:12 -0700

    remove eslint rule and changed img to nextImage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Footer() {
     return (
         <footer className="bg-[var(--off-black)] text-gray-200 py-8 mt-[60px]">
@@ -5,11 +7,11 @@ export default function Footer() {
                 {/* Logo and Navigation */}
                 <div className="flex flex-col justify-center md:flex-row items-center justify-between gap-4 md:gap-8">
                     <div className="flex items-center gap-2">
-                        <img src="/images/websiteLogo.png" alt="Company Logo" className="w-10 h-10 rounded-[50px]" />
-                        <span className="text-xl font-semibold text-[var(--gray-300)]">Unfinished Pages</span>
+                        <Image src="/images/websiteLogo.png" alt="Company Logo" width={30} height={30} className="rounded-[50px]" />
+                        <span className="text-md font-semibold text-[var(--gray-300)]">Unfinished Pages</span>
                     </div>
                     {/* Footer Bottom Text */}
-                    <div className="text-center text-sm">
+                    <div className="text-center text-[.95rem]">
                         <p>&copy; {new Date().getFullYear()} Unfinished Pages. All rights reserved.</p>
                     </div>
                 </div>
