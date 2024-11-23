e40ba9a8974517b15c2750541d52d8bf1df2f33e
Anthony Barragan <anthony.bar.89@gmail.com>
2024-11-22 22:55:21 -0800

    removed not null to updated At

--- prisma/migrations/20241122184257_made_contentblock_reference_image_model_added_created_at_and_updated_at_for_blogs_and_images/migration.sql
+++ prisma/migrations/20241122184257_made_contentblock_reference_image_model_added_created_at_and_updated_at_for_blogs_and_images/migration.sql
@@ -7,7 +7,7 @@
 */
 -- AlterTable
 ALTER TABLE "Blog" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
 
 -- AlterTable
 ALTER TABLE "ContentBlock" DROP COLUMN "url",
