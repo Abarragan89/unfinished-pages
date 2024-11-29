a2075c2f6ede45624fcfd68cd189edbb79cec9a6
Anthony Barragan <anthony.bar.89@gmail.com>
2024-11-28 23:41:51 -0800

    added publish, unpublish, and delete btns and routes. added loaders to buttons and added error messages for users trying to post blog with no description or photo.

--- types/blog.d.ts
+++ types/blog.d.ts
@@ -8,10 +8,10 @@ export interface BlogData {
     pictureURL: string;
     likes: number;
     dislikes: number;
-    isPublished: boolean;
-    publishedDate: Date;
-    content: BlogContent;
-    readDuration: number;
+    isPublished?: boolean;
+    publishedDate?: Date;
+    content?: BlogContent;
+    readDuration?: number;
 }
 
 export interface NestedListChildren {
