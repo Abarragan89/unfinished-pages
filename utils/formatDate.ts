310afb788b1f6c50ddc9f845255fd2d2d2dc674e
Anthony Barragan <anthony.bar.89@gmail.com>
2024-12-26 20:04:22 -0800

    made a new date object in formate date

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -17,13 +17,14 @@ export function formatDate(date: Date | undefined): string {
     // });
     if (!date) return ''; // Return an empty string if date is undefined
 
-    const year = date.getFullYear();
+    const newDate = new Date(date)
+    const year = newDate.getFullYear();
     const monthNames = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December'
     ];
-    const month = monthNames[date.getMonth()]; // Get month name from the array
-    const day = date.getDate();
+    const month = monthNames[newDate.getMonth()]; // Get month name from the array
+    const day = newDate.getDate();
 
     return `${month} ${day}, ${year}`;
 }
