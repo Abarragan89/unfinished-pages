893901789c0f0eb16b7818e7b7084f17a75365e0
Anthony Barragan <anthony.bar.89@gmail.com>
2024-12-28 23:11:56 -0800

    increased the timeout to 25 seconds to update the blog content transaction because I was hitting 12 seconds with 7 minute blogs

--- src/app/api/authorRoutes/blog/[id]/blogContent/route.ts
+++ src/app/api/authorRoutes/blog/[id]/blogContent/route.ts
@@ -1,86 +1,5 @@
-// import { NextResponse, type NextRequest } from 'next/server';
-// import { prisma } from '../../../../../../../utils/prisma';
-// import { BlogContent } from '../../../../../../../types/blog';
-
-// export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-//     try {
-//         const userId = request.headers.get('x-user-id');
-//         if (!userId) {
-//             return NextResponse.json({ error: 'User ID is missing' }, { status: 401 });
-//         }
-
-//         const { id: blogId } = params;
-//         const { content, readDuration } = await request.json();
-
-//         // Wrap Prisma operations in a transaction
-//         await prisma.$transaction(async (prisma) => {
-//             // Delete all content blocks for the blog
-//             await prisma.contentBlock.deleteMany({
-//                 where: { blogId }
-//             });
-
-//             // Create new content blocks
-//             await Promise.all(
-//                 content.map(async (content: BlogContent, index: number) => {
-//                     return prisma.contentBlock.create({
-//                         data: {
-//                             blogId,
-//                             type: content.type, // paragraph, heading, etc.
-//                             imageId: content?.image?.id || null,
-//                             orderNumber: index,
-//                             videoUrl: content.videoUrl || null,
-//                             children: {
-//                                 create: content.children.map((detail) => ({
-//                                     url: detail.url || null,
-//                                     text: detail.text || '',
-//                                     bold: detail.bold || false,
-//                                     italic: detail.italic || false,
-//                                     underline: detail.underline || false,
-//                                     type: detail.type || null, // list-type or link
-//                                     children: detail.children ? {
-//                                         create: detail.children.map((item) => ({
-//                                             text: item.text || '',
-//                                             bold: item.bold || false,
-//                                             italic: item.italic || false,
-//                                             underline: item.underline || false,
-//                                             type: item.type || null, // this is for inline anchor tags in lists
-//                                             url: item.url || null,
-//                                             children: item.children ? {
-//                                                 create: item.children.map((item) => ({
-//                                                     text: item.text || '',
-//                                                 }))
-//                                             } : undefined,
-//                                         }))
-//                                     } : undefined,
-//                                 })),
-//                             },
-//                         },
-//                     });
-//                 })
-//             );
-//             // Update blog readDuration
-//             await prisma.blog.update({
-//                 where: { id: blogId },
-//                 data: {
-//                     readDuration
-//                 }
-//             });
-//         },
-//         {timeout: 20000}
-//     );
-
-//         // Return success response
-//         return NextResponse.json({ message: 'success' });
-//     } catch (error) {
-//         console.error('Error during blog content update:', error);
-//         // Return error response
-//         return NextResponse.json({ error: 'Something went wrong while updating the blog' }, { status: 500 });
-//     }
-// }
-
 import { NextResponse, type NextRequest } from 'next/server';
 import { prisma } from '../../../../../../../utils/prisma';
-import { BlogContent } from '../../../../../../../types/blog';
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
     try {
@@ -153,7 +72,7 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
                     readDuration
                 }
             });
-        }, { timeout: 20000 });
+        }, { timeout: 25000 });
 
         // Return success response
         return NextResponse.json({ message: 'success' });
